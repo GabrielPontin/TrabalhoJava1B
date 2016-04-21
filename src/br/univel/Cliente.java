af34cbcbab591091fc3e60526707095d390ab6c7
CLIENTE <CLIENTE@192.168.25.8>
2016-04-21 13:26:47 -0300

    Parte do Cliente.java

--- src/br/univel/Cliente.java
+++ src/br/univel/Cliente.java
@@ -1,51 +1,51 @@
 package br.univel;
 
-import br.univel.Coluna;
-import br.univel.Tabela;
-
-	public class Cliente {
-
-		@Coluna(pk=true, nome="CADID")
-		private int id;
-
-		@Coluna(nome="CADNOME")
-		private String nome;
-
-		public Cliente() {
-			this(0, null);
-		}
-
-		public Cliente(int id, String nome) {
-			super();
-			this.id = id;
-			this.nome = nome;
-		}
-
-
-		public void dizerOla() {
-			System.out.println("Olá!");
-		}
-
-		public String retornarOla(String nome) {
-			return "Olá " + nome + "!";
-		}
-
-		public int getId() {
-			return id;
-		}
-
-		public void setId(int id) {
-			this.id = id;
-		}
-
-		public String getNome() {
-			return nome;
-		}
-
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-
+@Tabela("CAD_CLIENTE")
+public class Cliente {
+	@Coluna(pk=true)
+	private int id;
+	@Coluna(nome="CLNOME")
+	private String nome;
+	@Coluna(nome="CLENDEREÇO")
+	private String endereço;
+	@Coluna(nome="CLTELEFONE")
+	private String telefone;
+	//private EstadoCivil estadoCivil = EstadoCivil.CASADO;
+	public Cliente() {
+		this(0, null, null, null, null);
 	}
 
-
+	public Cliente(int id, String nome, String endereço, String telefone, Object object) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.nome = endereço;
+		this.nome = telefone;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereço() {
+		return endereço;
+	}
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+		
+}
