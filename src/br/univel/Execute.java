f68f7c6cc13bc2e73022a5db0b95b89810686876
CLIENTE <CLIENTE@192.168.25.8>
2016-04-25 12:46:09 -0300

    Classe Executa com os atributos do programa

--- src/br/univel/Execute.java
+++ src/br/univel/Execute.java
@@ -2,4 +2,24 @@ package br.univel;
 
 public class Execute {
 
+	public static void main(String[] args) {
+		Dao d = new Daoimpl();
+		Cliente c = new Cliente();
+		c.setId(1);
+		c.setNome("Pontin");
+		c.setTelefone("123456789");
+		c.setEndereço("Rua Tal");
+		
+		d.excluir(c);
+		d.create(c);
+		d.salvar(c);
+		d.salvar(c);
+		d.salvar(c);
+		d.listarTodos();
+		d.buscar(c);
+		d.atualizar(c);
+		d.delete(c);
+		d.listarTodos();		
+	}
+	
 }
