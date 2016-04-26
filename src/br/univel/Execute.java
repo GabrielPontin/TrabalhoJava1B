944cd2cafc2d6bf626400e536d944af46b2a2b8d
CLIENTE <CLIENTE@192.168.25.8>
2016-04-26 16:36:29 -0300

    Mudanças na Class Executa dos Clientes

--- src/br/univel/Execute.java
+++ src/br/univel/Execute.java
@@ -1,20 +1,20 @@
 package br.univel;
 
+import java.sql.SQLException;
+
 public class Execute {
 
-	public static void main(String[] args) {
-		Dao d = new Daoimpl();
-		Cliente c = new Cliente();
-		c.setId(1);
-		c.setNome("Pontin");
-		c.setTelefone("123456789");
-		c.setEndereço("Rua Tal");
+	public static void main(String[] args) throws SQLException {
+		Dao<Cliente, Cliente> d = new Daoimpl<Cliente, Cliente>();
+		Cliente c = new Cliente(1,"Pontin","0001","Rua 10", EstadoCivil.SOLTEIRO);
+		Cliente c2 = new Cliente(1,"Gibimba","0002","Rua 05", EstadoCivil.CASADO);
+		Cliente c3 = new Cliente(1,"Alakart","0003","Rua 02", EstadoCivil.VIUVO);
 		
 		d.excluir(c);
 		d.create(c);
 		d.salvar(c);
-		d.salvar(c);
-		d.salvar(c);
+		d.salvar(c2);
+		d.salvar(c3);
 		d.listarTodos();
 		d.buscar(c);
 		d.atualizar(c);
