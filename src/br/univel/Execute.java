5d7effd6de1f1e5b240803ecedfe125bb2915bd1
CLIENTE <CLIENTE@192.168.25.8>
2016-04-27 22:28:49 -0300

    Ajustando os dados para o banco de dados

--- src/br/univel/Execute.java
+++ src/br/univel/Execute.java
@@ -1,25 +1,96 @@
 package br.univel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
-public class Execute {
+import br.univel.EstadoCivil;
 
-	public static void main(String[] args) throws SQLException {
-		Dao<Cliente, Cliente> d = new DaoImpl<Cliente, Cliente>();
-		Cliente c = new Cliente(1,"Pontin","0001","Rua 10", EstadoCivil.SOLTEIRO);
-		Cliente c2 = new Cliente(1,"Gibimba","0002","Rua 05", EstadoCivil.CASADO);
-		Cliente c3 = new Cliente(1,"Alakart","0003","Rua 02", EstadoCivil.VIUVO);
+public class Execute extends SqlGenImpl{
+
+	Connection con;
+	
+
+	public Execute() throws SQLException{
+
+		AbrirConexao();
 		
-		d.excluir(c);
-		d.create(c);
-		d.salvar(c);
-		d.salvar(c2);
-		d.salvar(c3);
-		d.listarTodos();
-		d.buscar(c);
-		d.atualizar(c);
-		d.delete(c);
-		d.listarTodos();		
+		//Istancia a classe generica ImplDao com o vetor para armazenar os objetos Clientes
+		DaoImpl<Cliente, Integer> clientes = new DaoImpl<>();
+		
+		//Cria a tabela 
+		String strCreateTable = getCreateTable(con, new Cliente());
+		
+		//Istancia os objetos a serem inseridos
+		Cliente c1 = new Cliente(1, "Gabriel", "Avenida 01", "0000-1111", EstadoCivil.SOLTEIRO.getidestado());
+		Cliente c2 = new Cliente(2, "Gibimba", "Rua 02", "0000-2222", EstadoCivil.CASADO.getidestado());
+		Cliente c3 = new Cliente(3, "Alakart", "Avenida 03", "0000-3333", EstadoCivil.VIUVO.getidestado());
+		
+		System.out.println("");
+		//Insere 3 objetos Clientes
+		clientes.Salvar(c1);
+		clientes.Salvar(c2);
+		clientes.Salvar(c3);
+		
+		//Insere os 3 objetos instanciados na tabela criada
+		PreparedStatement ps;
+		ps = getSqlInsert(con, c1);
+		ps.addBatch();
+		ps = getSqlInsert(con, c2);
+		ps.addBatch();
+		ps = getSqlInsert(con, c3);
+		ps.addBatch();
+		ps.executeBatch();
+		
+		//Lista os 3 objetos presentes no banco
+		PreparedStatement pl = getSqlSelectAll(con, c1);
+		clientes.listarTodos();
+		
+		//Busca o objeto id 1
+		PreparedStatement pb = getSqlSelectById(con, c1);	
+		clientes.buscar(1);
+		
+		//Altera o objeto id 1
+		c1.setNome("Gabriel");
+		clientes.atualizar(c1);
+		PreparedStatement pu = getSqlUpdateById(con, c1);	
+
+		//Remove o objeto id 2
+		PreparedStatement pr = getSqlDeleteById(con, c1);
+		clientes.excluir(2);
+
+		//Lista todos os objetos novamente
+		PreparedStatement pf = getSqlSelectAll(con, c1);
+		clientes.listarTodos();
+		
+		ps.close();
+		pl.close();
+		pb.close();
+		pu.close();
+		pr.close();
+		pf.close();
+		con.close();
+
+	}
+
+	private void AbrirConexao(){
+			try{
+				String url = "jdbc:h2:./BancoPontin";
+				String user = "sa";
+				String pass = "sa";
+				con = DriverManager.getConnection(url, user, pass);	
+				System.out.println("Conexão Realizada!");				
+			}catch(SQLException e){
+				System.out.println("Erro ao realizar a conexão com o banco de dados!");
+			}
+
 	}
 	
+	public static void main(String[] args) throws SQLException{
+		System.out.println("-----------------|Mecânismo Genérico para persistência de objetos|-----------------\n");
+		new Execute();
+	}
+		
 }
